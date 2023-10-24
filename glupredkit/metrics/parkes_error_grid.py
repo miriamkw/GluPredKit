5a43648cfef49847fa194ed6b10a004f8c5484fa
Miriam K. Wolff <miriamkwolff@hotmail.com>
2023-10-24 11:42:45 +0200

    #33 - Moved error grid tables to metrics from plots

---
+++ glupredkit/metrics/parkes_error_grid.py
@@ -0,0 +1,13 @@
+from .base_metric import BaseMetric
+from error_grids import zone_accuracy
+
+
+class Metric(BaseMetric):
+    def __init__(self):
+        super().__init__('Parkes Error Grid')
+
+    def __call__(self, y_true, y_pred):
+        accuracy_values = zone_accuracy(y_true, y_pred, 'parkes')
+        formatted_values = ["{:.1f}%".format(value * 100) for value in accuracy_values]
+
+        return formatted_values
