4f5acb90f48c7df52f32cf0d2b6552f44154e121
Miriam K. Wolff <miriamkwolff@hotmail.com>
2023-11-01 15:01:23 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 project_name = "GluPredKit"
-version = "0.1.0"
+version = "0.1.1"
 author = "Miriam K. Wolff"
 author_email = "miriamkwolff@outlook.com"
 package_name = "glupredkit"  # The package name on pip install
