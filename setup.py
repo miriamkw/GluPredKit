e5da621651aa54eb88a7f8b26d086f6f198c9a4e
Miriam K. Wolff <miriamkwolff@hotmail.com>
2023-11-28 13:38:25 +0100

    Bumping version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 project_name = "GluPredKit"
-version = "0.1.6"
+version = "0.1.7"
 author = "Miriam K. Wolff"
 author_email = "miriamkwolff@outlook.com"
 package_name = "glupredkit"  # The package name on pip install
