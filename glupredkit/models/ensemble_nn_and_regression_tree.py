3a95e6dd73c3030b89b6cd8508203de4e1841ff1
do-develop <doyoh1179@gmail.com>
2024-01-11 23:39:56 +0900

    fix: prep_nn corrected

--- glupredkit/models/ensemble_nn_and_regression_tree.py
+++ glupredkit/models/ensemble_nn_and_regression_tree.py
@@ -13,31 +13,32 @@ from .base_model import BaseModel
 
 class Model(BaseModel):
     def __init__(self, prediction_horizon):
-        self.ph = prediction_horizon
+        self.ph = prediction_horizon // 5 # 5 minute intervals
         self.window = 16
         self.num_iteration = 10 # as per the source code, may need adjustment
         self.snn_model = None
         self.eim_model = RandomForestRegressor(n_estimators=100)
+        self.best_indices = None
 
     def fit(self, X_train, Y_train):
-        self.snn_model = self._create_shallow_nn(X_train.shape[1:])
-        # for _ in range(self.num_iteration):
-        # X = self._prep_nn(X_train, self.window, self.ph)
-        self.snn_model.fit(X_train, Y_train, epochs=10, batch_size=32, validation_split=0.2, verbose=1)
-        # Train corrective model for each iteration
-        cgm_hat_train = self.snn_model.predict(X_train)
-        cgm_array = Y_train.values
-        # max_features = 35
-        error = cgm_array - cgm_hat_train.flatten()
-        self._train_error_imputation_module(X_train, error)
+        X_prep = self._prep_nn(X_train.iloc[:,0], self.window)
+        self.snn_model = self._create_shallow_nn(X_prep.shape)
+        self.snn_model.fit(X_prep, Y_train, epochs=200, validation_split=0.2, batch_size=32, verbose=1)
+        # cgm_hat_train = self.snn_model.predict(X_prep)
+        # cgm_array = Y_train.values
+        # error = cgm_array - cgm_hat_train.flatten()
+        # self._train_error_imputation_module(X_train, error)
 
         return self
 
     def predict(self, X_test):
-        snn_predictions = self.snn_model.predict(X_test)
-        eim_predictions = self.eim_model.predict(X_test)
-        final_predictions = self._combine_predictions(snn_predictions, eim_predictions)
-        return final_predictions
+        X_prep = self._prep_nn(X_test.iloc[:,0], self.window)
+        snn_predictions = self.snn_model.predict(X_prep)
+        # # print('snn_predictions:', snn_predictions)
+        # eim_predictions = self.eim_model.predict(X_test.values[:, self.best_indices])
+        # # print('eim_predictions:', eim_predictions)
+        # final_predictions = self._combine_predictions(snn_predictions, eim_predictions)
+        return snn_predictions
 
     def best_params(self):
         # Return the best parameters found by GridSearchCV
@@ -53,7 +54,7 @@ class Model(BaseModel):
     def _create_shallow_nn(self, input_shape):
         model = Sequential()
         # Add layers to the neural network
-        model.add(Dense(64, input_shape=input_shape, activation='relu'))
+        model.add(Dense(64, input_shape=(None, 12529, 16), activation='relu'))
         model.add(Dense(32, activation='relu'))
         model.add(Dense(1, activation='linear'))  # Output layer for predicting BG values
         # Compile the model
@@ -68,7 +69,7 @@ class Model(BaseModel):
         return model
 
     def _train_error_imputation_module(self, features, error):
-        # max_features = 35
+        max_features = 35
         # error = features_array - target.flatten()  # Calculating error from features and target
         
         # feature ranking using ReliefF
@@ -77,19 +78,14 @@ class Model(BaseModel):
         else:
             feature_selector = ReliefF(n_neighbors=30)
 
-        # print("values ", features.values)
-        # print("reshaped ", features.values.reshape(-1, 1))
         feature_weights = feature_selector.fit(features.values, error).feature_importances_
         ranked_indices = np.argsort(feature_weights)[::-1]  # Reverse sort indices
         
-        print("ranked_indices: ", ranked_indices)
+        # print("ranked_indices: ", ranked_indices)
         # best_feature_indices = ranked_indices[:max_features]
-        best_feature_indices = ranked_indices[:]
-        
-        selected_features = features.values[:, best_feature_indices]
+        self.best_indices = ranked_indices[:max_features]
+        selected_features = features.values[:, self.best_indices]
 
-        # model training (using RandomForestRegressor)
-        rf_regressor = RandomForestRegressor(n_estimators=100)
         self.eim_model.fit(selected_features, error)
         
         return self.eim_model
@@ -101,8 +97,11 @@ class Model(BaseModel):
     takes a sequential data stream, breaks it down into smaller segments of a defined window size, 
     arranges segments into a structured format for neural networks.
     '''
-    def _prep_nn(self, signal, ord_val, ph_val):
-        X = np.zeros((len(signal) - ord_val - ph_val + 1, ord_val))
-        for k in range(ord_val, len(signal) - ph_val + 1):
-            X[k - ord_val] = signal[k - ord_val:k]
+    def _prep_nn(self, data_stream, window):
+        X = np.zeros((len(data_stream), window))
+        for i in range(0, window):
+            X[i] = ([data_stream[i]] * i) + ([data_stream[i]] * (window - i))
+        for k in range(window, len(data_stream)):
+            X[k] = data_stream[k - window:k]
         return X
+ 
