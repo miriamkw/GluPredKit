1a27e1735d2c297fc32fd35363ab997de64b4eeb
do-develop <doyoh1179@gmail.com>
2024-03-14 00:01:17 +0100

    feat: participants activity logs are updated

--- glupredkit/metrics/rmse_period.py
+++ glupredkit/metrics/rmse_period.py
@@ -3,20 +3,43 @@ import numpy as np
 import pandas as pd
 from glupredkit.helpers.unit_config_manager import unit_config_manager
 
-'''
-# Eirik's
-activity_logs = [{'start_time': "2024-02-03 13:30", 'duration': 2, 'activity': 'cross-country skiing and snow-shoveling'},
+
+# perticipant 1
+activity_logs = [{'start_time': "2024-01-26 21:00", 'duration': 1, 'activity': 'cross-country skiing'},
+                 {'start_time': "2024-01-27 22:45", 'duration': 0.75, 'activity': 'biking'},
+                 {'start_time': "2024-01-30 23:00", 'duration': 0.5, 'activity': 'strength training'},
+                 {'start_time': "2024-01-31 20:15", 'duration': 0.75, 'activity': 'biking, walking, and snow shoveling'},
+                 {'start_time': "2024-02-01 21:00", 'duration': 1, 'activity': 'snow-shoveling'},
+                 {'start_time': "2024-02-02 22:30", 'duration': 0.25, 'activity': 'snow-shoveling'},
+                 {'start_time': "2024-02-03 13:30", 'duration': 2, 'activity': 'cross-country skiing and snow-shoveling'},
                  {'start_time': "2024-02-03 20:00", 'duration': 1, 'activity': 'snow-shoveling'},
                  {'start_time': "2024-02-04 13:00", 'duration': 0.5, 'activity': 'snow-shoveling'},
                  {'start_time': "2024-02-04 23:30", 'duration': 0.5, 'activity': 'snow-shoveling'},
                  {'start_time': "2024-02-05 22:00", 'duration': 0.5, 'activity': 'cross-country skiing'}
                  ]
 '''
-activity_logs = [{'start_time': "2024-02-17 17:00", 'duration': 0.67, 'activity': 'walk'},
+# participant 2
+activity_logs = [{'start_time': "2024-02-12 09:30", 'duration': 0.5, 'activity': 'walk'},
+                 {'start_time': "2024-02-12 12:30", 'duration': 0.5, 'activity': 'walk'},
+                 {'start_time': "2024-02-12 16:25", 'duration': 0.3, 'activity': 'walk'},
+                 {'start_time': "2024-02-12 19:55", 'duration': 0.25, 'activity': 'walk'},
+                 {'start_time': "2024-02-12 23:10", 'duration': 0.3, 'activity': 'walk'},
+
+                 {'start_time': "2024-02-13 07:50", 'duration': 0.3, 'activity': 'walk'},
+                 {'start_time': "2024-02-13 09:50", 'duration': 0.3, 'activity': 'walk'},
+
+                 {'start_time': "2024-02-14 14:15", 'duration': 0.3, 'activity': 'walk'},
+
+                 {'start_time': "2024-02-15 07:50", 'duration': 0.2, 'activity': 'walk'},
+                 {'start_time': "2024-02-15 13:40", 'duration': 0.5, 'activity': 'walk'},
+                 {'start_time': "2024-02-15 17:40", 'duration': 0.3, 'activity': 'walk'},
+
+                 {'start_time': "2024-02-17 17:00", 'duration': 0.67, 'activity': 'walk'},
+
                  {'start_time': "2024-02-18 17:30", 'duration': 0.5, 'activity': 'walk'},
                  {'start_time': "2024-02-18 22:00", 'duration': 0.5, 'activity': 'walk'}
                  ]
-
+'''
 class Metric(BaseMetric):
     def __init__(self):
         super().__init__('RMSE')
