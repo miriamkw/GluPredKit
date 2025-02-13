ddbd420743c1ae1d2a591f9bd3b31e7cd8e7e67b
Miriam K. Wolff <miriamkwolff@hotmail.com>
2025-02-13 14:44:00 +0100

    Added preprocessor that doesnt interpolate or remove nans

--- glupredkit/helpers/cli.py
+++ glupredkit/helpers/cli.py
@@ -19,7 +19,7 @@ from ..helpers.model_config_manager import ModelConfigurationManager
 def read_data_from_csv(input_path, file_name):
     input_path = Path(input_path)
     file_path = input_path / file_name
-    return pd.read_csv(file_path, index_col="date", parse_dates=True)
+    return pd.read_csv(file_path, index_col="date", parse_dates=True, low_memory=False)
 
 
 def store_data_as_csv(df, output_path, file_name):
